5ddb45e33c415c69b017aa4f002d11dfff35c6b6
fight <fight@163.com>
2020-06-12 17:13:39 +0800

    提交getTest

---
+++ gitTest/src/fight/git/GitTest.java
@@ -0,0 +1,9 @@
+package fight.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("用力");
+        System.out.println("舒服");
+    }
+}
